514f1911d6bdccaf0bca5107e1242a8f5dcffae5
Abubakar <118282182+abubakarp789@users.noreply.github.com>
2024-06-14 17:18:29 +0500

    Task 2 of Day 4
    Knapsack Algorithm modified

--- Day4/knapsack_Algorithm.py
+++ Day4/knapsack_Algorithm.py
@@ -1,6 +1,6 @@
 def knapsack(weights, values, capacity):
     n = len(weights)
-    dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
     for i in range(1, n + 1):
         for j in range(1, capacity + 1):
             if weights[i - 1] <= j:
@@ -9,8 +9,7 @@ def knapsack(weights, values, capacity):
                 dp[i][j] = dp[i - 1][j]
     return dp[n][capacity]
 
-# Test the knapsack function with an example set of weights and values
-weights = [1, 3, 4,5]
+weights = [1, 3, 4, 5]
 values = [1, 4, 5, 7]
 capacity = 7
 print(knapsack(weights, values, capacity))
