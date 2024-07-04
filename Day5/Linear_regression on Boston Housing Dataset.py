bd8c40b002b54ffa1927d1f5c125e2befa387c06
Abubakar <118282182+abubakarp789@users.noreply.github.com>
2024-07-04 21:23:56 +0500

    Day 9 Task

--- Day5/Linear_regression on Boston Housing Dataset.py
+++ Day5/Linear_regression on Boston Housing Dataset.py
@@ -3,7 +3,6 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 # (a). Load the Boston Housing dataset
